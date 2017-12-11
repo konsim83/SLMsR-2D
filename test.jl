6e74b80a167996538586e5634ee208ac7fcddd57
Konrad Simon <konrad.simon@uni-hamburg.de>
2017-12-11 17:36:36 +0100

    Clean up assembly routines.

--- test.jl
+++ test.jl
@@ -1,57 +1,58 @@
-#=
-include("Parameter.jl")
-include("Problem.jl")
-include("Quad.jl")
-include("Mesh.jl")
-include("FEM.jl")
-include("Time_integrator.jl")
-include("Solver.jl")
-=#
+if false
+    include("Parameter.jl")
+    include("Problem.jl")
+    include("Quad.jl")
+    include("Mesh.jl")
+    include("FEM.jl")
+    include("Time_integrator.jl")
+    include("Solver.jl")
+end
 
-n_edge_per_seg = 10
+n_edge_per_seg = 120
 n_order_quad = 4
 n_order_FEM = 1
 
 
-println("Start timing problem setup...")
-@time begin
-    println("\tMeshing...")
-    m = Mesh.mesh_unit_square(n_edge_per_seg);
-
-    println("\tReference element...")
-    r = FEM.RefEl_Pk{n_order_FEM}();
-    
-    println("\tDof handler...")
-    d = FEM.Dof_Pk_periodic_square{n_order_FEM}(m);
-    
-    println("\tProblem structure...")
-    p = Problem.Gaussian(1.0);
-    
-    println("\tQuadrature...")
-    q = Quad.Quad_simplex(n_order_quad);
-    
-    par = Parameter.Parameter_FEM(1.0,
-                                  1.0/1000,
-                                  n_edge_per_seg,
-                                  n_order_FEM,
-                                  n_order_quad,
-                                  1)
-    
-    #sol = FEM.Solution_FEM(d, par)
-    
-    println("\tTime integrator...")
-    tstep = Time_integrator.ImplEuler{Time_integrator.System_data_implEuler_ADE}(d, m, p)
+if true
+    println("Start timing problem setup...")
+    @time begin
+        println("\tMeshing...")
+        m = Mesh.mesh_unit_square(n_edge_per_seg);
+        
+        println("\tReference element...")
+        r = FEM.RefEl_Pk{n_order_FEM}();
+        
+        println("\tDof handler...")
+        d = FEM.Dof_Pk_periodic_square{n_order_FEM}(m);
+        
+        println("\tProblem structure...")
+        p = Problem.Gaussian(1.0);
+        
+        println("\tQuadrature...")
+        q = Quad.Quad_simplex(n_order_quad);
+        
+        par = Parameter.Parameter_FEM(1.0,
+                                      1.0/1000,
+                                      n_edge_per_seg,
+                                      n_order_FEM,
+                                      n_order_quad,
+                                      1)
+        
+        #sol = FEM.Solution_FEM(d, par)
+        
+        println("\tTime integrator...")
+        tstep = Time_integrator.ImplEuler{Time_integrator.System_data_implEuler_ADE}(d, m, p)
+    end
+    println("----------------------------\n")
 end
-println("----------------------------\n")
-
 # ===============================================
 
 
 
 
 println("Standard assembly...")
-#@time M = FEM.assemble_mass(m, d, r, q, par, p, 1)
-@time A = FEM.assemble_advection(m, d, r, q, par, p, 1)
+@time M = FEM.assemble_mass(m, d, r, q, par, p, 1)
+#@time A = FEM.assemble_advection(m, d, r, q, par, p, 1)
 #@time D = FEM.assemble_diffusion(m, d, r, q, par, p, 1)
 println("---------------------------\n\n")
 
@@ -62,13 +63,13 @@ ind_test = vec(transpose(repmat(i, 1, size(i,2))))
 lin = sub2ind((d.n_true_dof,d.n_true_dof), ind_test, ind)
 
 
-#M1 = sparse(ind_test, ind, zeros(Float64, length(ind)), d.n_true_dof, d.n_true_dof)
-A1 = sparse(ind_test, ind, zeros(Float64, length(ind)), d.n_true_dof, d.n_true_dof)
+M1 = sparse(ind_test, ind, zeros(Float64, length(ind)), d.n_true_dof, d.n_true_dof)
+#A1 = sparse(ind_test, ind, zeros(Float64, length(ind)), d.n_true_dof, d.n_true_dof)
 #D1 = sparse(ind_test, ind, zeros(Float64, length(ind)), d.n_true_dof, d.n_true_dof)
 #println("\n")
 
 println("In place assembly...")
-#@time FEM.assemble_mass!(M1, m, d, r, q, par, p, 1)
+@time FEM.assemble_mass!(M1, m, d, r, q, par, p, 1)
 #@time FEM.assemble_advection!(A1, m, d, r, q, par, p, 1)
 #@time FEM.assemble_diffusion!(D1, m, d, r, q, par, p, 1)
 println("---------------------------\n\n")
@@ -144,7 +145,7 @@ println("... Advection routines:")
 #DPhi_mesh = mesh_expand(DPhi, DF)
 
 
-A2 = sparse(d.ind_test, d.ind, zeros(Float64, length(d.ind)), d.n_true_dof, d.n_true_dof)
+#A2 = sparse(d.ind_test, d.ind, zeros(Float64, length(d.ind)), d.n_true_dof, d.n_true_dof)
 function adv2!(A  :: SparseMatrixCSC{Float64,Int64},
                m :: Mesh.TriMesh,
                d :: FEM.AbstractDof,
@@ -221,12 +222,12 @@ function adv3!(A  :: SparseMatrixCSC{Float64,Int64},
     
     return nothing
 end
-@time adv3!(A3, m, d, r, q, par, p)
+#@time adv3!(A3, m, d, r, q, par, p)
 
 
 
 
-#M3 = sparse(d.ind_test, d.ind, zeros(Float64, length(d.ind)), d.n_true_dof, d.n_true_dof)
+M3 = sparse(d.ind_test, d.ind, zeros(Float64, length(d.ind)), d.n_true_dof, d.n_true_dof)
 function mass3!(M  :: SparseMatrixCSC{Float64,Int64},
                       m :: Mesh.TriMesh,
                       d :: FEM.AbstractDof,
@@ -262,7 +263,7 @@ function mass3!(M  :: SparseMatrixCSC{Float64,Int64},
 
     return nothing
 end
-#@time mass3!(M3, m, d, r, q, par, p)
+@time mass3!(M3, m, d, r, q, par, p)
 
 println("\n")
 #-------------------------------------------------------------
